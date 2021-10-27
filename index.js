86bdb055badb1eb6e61f772bf3de660bc90729b7
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2021-10-28 00:47:13 +0600

    port added

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(express.json())
